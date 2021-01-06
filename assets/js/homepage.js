99d6ecbb5db7ca2ed4ed7fee2c5887b687e504e4
Brock <bruceanthonyii@gmail.com>
2021-01-05 23:40:11 -0600

    added search by languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 var getUserRepos = function (user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -23,6 +25,20 @@ var getUserRepos = function (user) {
     .catch(function(error) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect to GitHub");
+    });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });    
+        } else {
+          alert("Error: " + response.statusText);
+        }
     });
 };
 
@@ -71,7 +87,7 @@ var displayRepos = function(repos, searchTerm) {
 
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
-  }
+    }
     
     console.log(repos);
     console.log(searchTerm);
@@ -92,5 +108,19 @@ var formSubmitHandler = function(event) {
 
     // console.log(event);
 };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    // console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
